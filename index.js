e2296366b9ebaf8ec6ad79e84966e582461ff39e
aaafasf <csc.calle@yavirac.edu.ec>
2025-10-24 16:49:34 -0500

    Agregar servidor HTTP para Docker

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const http = require('http');
+
+// Funciones originales
 function sumar(a, b) {
   return a + b;
 }
@@ -12,6 +15,14 @@ function calculadora(op, a, b) {
   throw new Error('Operación no válida');
 }
 
-module.exports = { sumar, resta, calculadora };
+// Servidor HTTP para Docker
+const PORT = 3000;
 
-console.log("Resultado final (2+3):", sumar(2, 3));
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(`2+3 = ${sumar(2, 3)}\n5-2 = ${resta(5,2)}`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
